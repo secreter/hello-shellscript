53929ce9c350792b86ed1460557b53c15b42275f
northPole <so@redream.cn>
2018-05-28 14:43:41 +0000

    first commit

---
+++ sh1.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+PATH=${PATH}:/tmp
+export PATH
+echo -e "hello world \a \n"
+exit 0
---
+++ sh2.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+read -p "Please input your frist name:" fristname
+read -p "Please input your last name:" lastname
+echo -e "\n Your full name is " $fristname $lastname
